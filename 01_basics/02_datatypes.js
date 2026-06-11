e44ad351c19fd6996db52c4e55c1234975f18560
rushikeshshirke1107 <rushikeshshirke1107@gmail.com>
2026-06-11 12:42:30 +0000

    learned datatypes in javascript

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+let num=132
+let name="raj"
+console.table([num,name])
+console.log(typeof num)
+console.log(typeof name)
+const val= "125";
+console.log(typeof val)
+// in javascript there are so many different datatypes primary are
+//number,boolean= true/false, string,bigint,
+//null= it is empty and typeof is object
+// undefined
+//symbol
