c97eb9fe58b38556b39c215c1f8235e6cc734de3
rushikeshshirke1107 <rushikeshshirke1107@gmail.com>
2026-06-11 12:22:45 +0000

    learned variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+let user_Name=1223;
+const password="Pass@123"
+var state="maharashtra"     // in updates version avoid writing var beacuse block Scope issues
+country="india"             //avoid writing like this 
+
+console.table([user_Name,password,state,country])
+user_name="raj"
+//password="pass@343"         // we cant change constant declaired value
+console.table([user_Name,password,state,country])
