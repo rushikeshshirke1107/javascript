a2476920a4dbd7f401c2ed6ce565001753969eab
rushikeshshirke1107 <rushikeshshirke1107@gmail.com>
2026-06-12 05:58:39 +0000

    type conversion

---
+++ 01_basics/03_typeCoversion.js
@@ -0,0 +1,13 @@
+let num=12
+console.log(typeof(num))
+
+const txt=String(num);
+console.log(typeof(txt));
+
+let txt1="Raha"
+console.log(typeof(txt1))
+
+let txt1Isboolean=Boolean(txt1)
+console.log(typeof(txt1Isboolean))
+console.log(txt1Isboolean)
+console.log(txt1)
