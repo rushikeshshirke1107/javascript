f38b1344dcce1fdb9a440dc8ba0c03221621d223
rushikeshshirke1107 <rushikeshshirke1107@gmail.com>
2026-06-12 07:27:46 +0000

    learned operations

--- 01_basics/03_typeCoversion.js
+++ 01_basics/03_typeCoversion.js
@@ -1,13 +1,49 @@
-let num=12
-console.log(typeof(num))
+// let num=12
+// console.log(typeof(num))
 
-const txt=String(num);
-console.log(typeof(txt));
+// const txt=String(num);
+// console.log(typeof(txt));
 
-let txt1="Raha"
-console.log(typeof(txt1))
+// let txt1="Raha"
+// console.log(typeof(txt1))
 
-let txt1Isboolean=Boolean(txt1)
-console.log(typeof(txt1Isboolean))
-console.log(txt1Isboolean)
-console.log(txt1)
+// let txt1Isboolean=Boolean(txt1)
+// console.log(typeof(txt1Isboolean))
+// console.log(txt1Isboolean)
+// console.log(txt1)
+
+//**********************operrations************************************* */
+
+// let num1=2
+// let num2=3
+// console.log(num1+num2);
+
+// let num3="2"
+// let num4=6
+// let num5=8
+// console.log(num3+num4);
+// console.log(num4+num3);
+// console.log(num5+num4+num3)
+// console.log(num3+num4+num5);// if the first value is String then it consider all values as string
+
+// console.log(num1+num2);
+// console.log(num1-num2);
+// console.log(num1*num2);
+// console.log(num1/num2);
+// console.log(num1%num2);
+
+// console.log(2**3);
+
+//*******************increment**************** */
+
+let a=4
+let b=a
+console.log(a++)
+console.log(b);
+
+console.log(++a);
+console.log(b);
+
+
+
+
