343b7cccd9827a256740c13e1b2d9f48203a2f91
rushikeshshirke1107 <rushikeshshirke1107@gmail.com>
2026-06-12 07:52:21 +0000

    comparison operator

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,29 @@
+console.log(1>2);
+console.log(2<1);
+console.log(5>=5);
+console.log(6<=10);
+
+console.log(undefined>0);
+console.log(undefined<0);
+
+console.log(null>0);
+console.log(null<0);
+console.log(null<=0);
+console.log(null>=0);
+
+// === strictly checks values with datatypes
+
+console.log("3"<1);
+console.log("5">=5) //here it automatically converts values and checks
+console.log("5"===5); //it strictly checks values as well as data types
+
+
+
+
+
+
+
+
+
+
+
