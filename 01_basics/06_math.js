49b81520504e1c9005dc3a28dff643dd66aecd04
rushikeshshirke1107 <rushikeshshirke1107@gmail.com>
2026-06-13 12:28:20 +0000

    learned mathematic operations in js

---
+++ 01_basics/06_math.js
@@ -0,0 +1,31 @@
+let num=100
+let num2= new Number(1000000);
+console.log(num);
+console.log(num2);
+
+console.log(num.toString().length)
+console.log(num2.toLocaleString(`en-In`));
+console.log(num2.toFixed(2));
+console.log(num2.toPrecision(4));
+
+// **********************************MATHS*********************************
+console.log(Math.SQRT1_2);
+console.log(Math.max(4,8,9,7,6));
+console.log(Math.min(8,5,4,6,72,1));
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log(Math.random()*10+1);
+console.log(Math.round(15.2));
+console.log(Math.ceil(15.2));
+console.log(Math.floor(15.2));
+let max=10;
+let min=5;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
+
+
