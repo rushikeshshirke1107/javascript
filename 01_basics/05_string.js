856df359f6f43c00003052fd50d3c08937f3615d
rushikeshshirke1107 <rushikeshshirke1107@gmail.com>
2026-06-13 06:25:24 +0000

    learned string functions

---
+++ 01_basics/05_string.js
@@ -0,0 +1,9 @@
+let name="jeevan"
+let surname="pawar"
+console.log(`my name is ${name} and my surname is ${surname}`);
+console.log(name.toUpperCase());
+console.log(surname.toLowerCase())
+console.log(surname.charAt('2'));
+console.log(name.length);
+console.log(surname.slice(2,3));
+console.log(name.concat(" is my name"))
