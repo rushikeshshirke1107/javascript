965cfc73c3f4ea29a56437d6d2a152a152edc4b3
rushikeshshirke1107 <rushikeshshirke1107@gmail.com>
2026-06-14 06:05:13 +0000

    learned date and time in js

---
+++ 01_basics/07_dateandTime.js
@@ -0,0 +1,22 @@
+// const date=new Date()
+// console.log(date.getDate());
+// console.log(date.getDay());
+// console.log(date.getFullYear());
+// console.log(date.getMonth());
+// console.log(date.getTimezoneOffset());
+
+
+// let myaCreatedDate= new Date("01-14-2023")
+// console.log(myaCreatedDate.toString());
+
+// let myaCreatedDate=new Date(2023,0,23)
+// console.log(myaCreatedDate);
+let myaCreatedDate=new Date(2023,0,23,5,3)
+console.log(myaCreatedDate);
+
+let myTimeStamp=Date.now()
+console.log(myTimeStamp);
+console.log(myaCreatedDate.getTime());
+
+
+
