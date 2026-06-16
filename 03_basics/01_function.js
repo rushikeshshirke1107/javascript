f8b4543e59994d0aa16f6531fe1759b71c6b2b83
rushikeshshirke1107 <rushikeshshirke1107@gmail.com>
2026-06-16 11:51:18 +0000

    learned how to give array and object as parameter.

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -18,3 +18,36 @@ function welcome(name="raj"){                   // default parameter
 }
 
 console.log(welcome("omkar"));
+
+function mycart(price){
+    console.log(price);
+}
+mycart(200);
+mycart(200,300,500);    // in this function returns only first value so we use spread operator.
+
+function myCart(...price){
+    console.log(`the price of the cart is ${price}`)
+}
+myCart(200,500,1000);      //spread operator assigns all values in single array.
+
+// Giving object as parameter
+
+const emp={
+    name:"raj",
+    salary:"54000"
+}
+function empDetails(employee){
+    return `name of the empolyee is ${employee.name} and salary is ${employee.salary}`
+}
+console.log(empDetails(emp));
+
+//giving array as parameter
+
+const arr=[200,500,752,888]
+
+function getarray(array){
+    console.log(array[2]);
+    
+    return array
+}
+console.log(getarray(arr));
