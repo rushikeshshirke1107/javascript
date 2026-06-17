f094263f302862938fb689232a4dbe0a26172f0a
rushikeshshirke1107 <rushikeshshirke1107@gmail.com>
2026-06-17 12:19:41 +0000

    learned Immediate Invoked Function Execution

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,13 @@
+function print(name){
+    console.log(`My name is ${name}`)
+}
+print("Jeevan");
+
+//*********************** IIME *********************************(
+
+(function myage(age){
+    console.log(`Age of Anuj is ${age}`);
+})(45);
+
+
+((num1,num2)=>console.log(num1+num2))(15,14)
