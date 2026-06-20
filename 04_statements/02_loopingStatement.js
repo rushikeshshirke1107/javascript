df95366d4396b4235de9a89e0e82f4a26f51e6d0
rushikeshshirke1107 <rushikeshshirke1107@gmail.com>
2026-06-20 08:29:35 +0000

    learned for loop

---
+++ 04_statements/02_loopingStatement.js
@@ -0,0 +1,63 @@
+// for(i=1;i<=10;i++){
+//     console.log("the number is: ",i)
+// }
+
+// while(i>=1){
+//     console.log("the number is :",i)
+//     i--;
+// }
+
+//or
+
+// let i=10
+// while(i>=1){
+//     console.log("the number is : ",i);
+//     if(i==1){
+//         break;
+//     }
+//     i--
+// }
+
+// // even numbers between 1 to 50
+
+// for(i=1;i<=50;i++){
+//     if(i%2==0){
+//         console.log(`${i} is even`);
+        
+//     }
+// }
+
+// // odd numbers between 1 to 50
+
+// for(i=1;i<=50;i++){
+//     if(i%2!=0){
+//         console.log(`${i} is odd`);
+        
+//     }
+// }
+
+// // sum of numbers between 1 to 100
+//   let sum=0
+// for(i=1;i<=100;i++){
+  
+//     sum+=i
+//     console.log(`sum of the number is ${sum}`);
+    
+// }
+
+//factorial of number between range 1 to 50
+fact=0
+for(i=0;i<=50;i++){
+    fact=fact+i
+    console.log("factorial are :",fact);
+    
+}
+
+// table of given number
+
+let num=5
+for(i=1;i<=10;i++){
+    mul=i*num
+    console.log(`${i}*${num}=${mul}`);
+    
+}
